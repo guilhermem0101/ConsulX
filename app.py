4fc4f02e01e4929aa4845c0d38430f6609d01159
guilhermem0101 <guilherme.sou@hotmail.com>
2025-10-10 22:45:55 -0300

    RECONSTRUÇÃO DO FRONT

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 import json
 from calcula_resultados import compute_indicators_from_files, extract_accounts
-
+from streamlit_elements import elements, mui, html, editor, nivo, media, lazy, sync, dashboard
 files = ["balancetes/balancete1.json"]
 result = compute_indicators_from_files(files)
 df_balancete = pd.json_normalize(
@@ -104,6 +104,8 @@ print("Tabela original:")
 print("\nTabela pivotada:")
 # tabela_pivot
 
+tabela_pivot = pd.merge(
+    tabela_pivot, df_indices["Disponibilidade de Caixa"], on='mes', how='inner')
 # Criar os cálculos
 tabela_pivot['Ativo_Total'] = tabela_pivot['ATIVO CIRCULANTE'] + \
     tabela_pivot['ATIVO NÃO CIRCULANTE']
@@ -112,7 +114,7 @@ tabela_pivot['Passivo_Total'] = tabela_pivot['PASSIVO CIRCULANTE'] + \
 
 tabela_pivot['Liquidez_Corrente'] = tabela_pivot['ATIVO CIRCULANTE'] / \
     tabela_pivot['PASSIVO CIRCULANTE']
-# tabela_pivot['Liquidez_Imediata'] = tabela_pivot['Disponibilidades'] / tabela_pivot['PASSIVO CIRCULANTE']
+tabela_pivot['Liquidez_Imediata'] = tabela_pivot['Disponibilidade de Caixa'] / tabela_pivot['PASSIVO CIRCULANTE']
 tabela_pivot['Liquidez_Geral'] = (tabela_pivot['ATIVO CIRCULANTE'] + tabela_pivot['ATIVO NÃO CIRCULANTE']) / \
     (tabela_pivot['PASSIVO CIRCULANTE'] +
      tabela_pivot['PASSIVO NÃO CIRCULANTE'])
@@ -123,6 +125,49 @@ tabela_pivot['Endividamento'] = tabela_pivot['Passivo_Total'] / \
 tabela_pivot['Endividamento_Geral'] = (
     tabela_pivot['Passivo_Total']) / tabela_pivot['Ativo_Total']
 
+ultima_data = tabela_pivot.index.max()
+indices_max = tabela_pivot.loc[tabela_pivot.index == ultima_data]
+
+
+# Dados de exemplo
+indicadores = [
+    {
+        "titulo": "Endividamento Geral",
+        "descricao": "Grau em que os Ativos Totais são financiados por recursos de terceiros.",
+        "memoria": "Passivo Circulante + Exigível a Longo Prazo / Ativo Total",
+        "valor": round(indices_max['Endividamento_Geral'].values[0], 2)
+    },
+    {
+        "titulo": "Endividamento",
+        "descricao": "Quanto a empresa possui de imediato no caixa, frente às obrigações exigíveis.",
+        "memoria": "Disponível / Passivo Circulante",
+        "valor": round(indices_max['Endividamento'].values[0], 2)
+    },
+    {
+        "titulo": "Liquidez Corrente",
+        "descricao": "Capacidade de a empresa saldar suas dívidas a curto prazo, em até 360 dias.",
+        "memoria": "Ativo Circulante / Passivo Circulante",
+        "valor": round(indices_max['Liquidez_Corrente'].values[0], 2)
+    },
+    {
+        "titulo": "Solvência",
+        "descricao": "Grau em que a empresa dispõe em Ativos Totais para o pagamento total de dívidas.",
+        "memoria": "Ativo Total / Passivo Circulante + Exigível a Longo Prazo",
+        "valor": round(indices_max['Solvencia_Geral'].values[0], 2)
+    },
+    {
+        "titulo": "Liquidez Geral",
+        "descricao": "Situação financeira da empresa a longo prazo, incluindo dívidas acima de 360 dias.",
+        "memoria": "(Ativo Circulante + Realizável a Longo Prazo) / (Passivo Circulante + Exigível a Longo Prazo)",
+        "valor": round(indices_max['Liquidez_Geral'].values[0], 2)
+    },
+    {
+        "titulo": "Liquidez Imediata",
+        "descricao": "Quanto a empresa possui de imediato no caixa, frente às obrigações exigíveis.",
+        "memoria": "Disponível / Passivo Circulante",
+        "valor": round(indices_max['Liquidez_Imediata'].values[0], 2)
+    }
+]
 # ======================== INIT ========================
 
 df_balancete.columns = df_balancete.columns.str.replace("indicators.", "")
@@ -131,10 +176,42 @@ receita_liquida = df_balancete.loc[0, "receita_liquida"]
 lucro_bruito = df_balancete.loc[0, "lucro_bruto"]
 lucro_liquido = df_balancete.loc[0, "lucro_liquido"]
 disponibilidade_caixa = df_balancete.loc[0, "disponibilidade_caixa"]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # ======================
 # CONFIGURAÇÕES GERAIS
 # ======================
 st.set_page_config(page_title="Dashboard Contábil", layout="wide")
+
 
 # ======================
 # SIDEBAR (MENU LATERAL)
@@ -149,7 +226,7 @@ with st.sidebar:
 # ======================
 # TOPO COM ABAS
 # ======================
-abas = st.tabs(["Gerencial", "Contábil", "Folha", "Balancete"])
+abas = st.tabs(["Gerencial", "Contábil", "Projeção", "Balancete"])
 
 with abas[1]:  # Aba "Contábil"
     st.subheader("📑 Painel Contábil")
@@ -176,7 +253,7 @@ with abas[1]:  # Aba "Contábil"
 
     st.markdown("---")
 
-   # ======================
+    # ======================
     # GRÁFICOS DE INDICADORES TEMPORAIS (BARRAS INDIVIDUAIS)
     # ======================
     st.subheader("📊 Indicadores de Liquidez e Endividamento ao Longo do Tempo")
@@ -196,7 +273,7 @@ with abas[1]:  # Aba "Contábil"
             text_auto=".2f",
             labels={"mes": "Mês", indicador: "Índice"},
             title=titulo,
-            color_discrete_sequence=["#FFFFFF"]
+            color_discrete_sequence=["#3E4DD8"]
         )
         fig.update_traces(marker_line_width=0.5)
         fig.update_layout(
@@ -208,58 +285,60 @@ with abas[1]:  # Aba "Contábil"
         return fig
 
 
-    # Criar duas colunas (2x2 + 1 embaixo)
-    col1, col2 = st.columns(2)
+   
 
+    # Primeira linha
+    col1, col2 = st.columns(2)
     with col1:
         st.plotly_chart(plot_indicator_bar(df_plot, "Liquidez_Corrente",
                         "Liquidez Corrente"), use_container_width=True)
+    with col2:
         st.plotly_chart(plot_indicator_bar(df_plot, "Solvencia_Geral",
                         "Solvência Geral"), use_container_width=True)
 
-    with col2:
+    # Segunda linha
+    col3, col4 = st.columns(2)
+    with col3:
         st.plotly_chart(plot_indicator_bar(df_plot, "Liquidez_Geral",
                         "Liquidez Geral"), use_container_width=True)
+    with col4:
+        st.plotly_chart(plot_indicator_bar(df_plot, "Liquidez_Imediata",
+                        "Liquidez Imediata"), use_container_width=True)
+
+    # Terceira linha
+    col5, col6 = st.columns(2)
+    with col5:
+        st.plotly_chart(plot_indicator_bar(df_plot, "Endividamento_Geral",
+                        "Endividamento Geral"), use_container_width=True)
+    with col6:
         st.plotly_chart(plot_indicator_bar(df_plot, "Endividamento",
                         "Endividamento"), use_container_width=True)
 
-    # Último gráfico centralizado
-    st.plotly_chart(plot_indicator_bar(df_plot, "Endividamento_Geral",
-                    "Endividamento Geral"), use_container_width=True)
-
-
-    # ======================
-    # GRÁFICOS DE INDICADORES TEMPORAIS (BARRAS INDIVIDUAIS)
-    # ======================
-    st.subheader("📊 Bignumbers ao Longo do Tempo")
-
-    # Converter índice para coluna e ordenar
-    df_plot_big = df_indices.reset_index().sort_values("mes")
-    df_plot_big["mes"] = pd.to_datetime(df_plot_big["mes"], format="%Y-%m")
-
     
-
-    # Criar duas colunas (2x2 + 1 embaixo)
-    col1, col2 = st.columns(2)
-
-    with col1:
-        st.plotly_chart(plot_indicator_bar(df_plot_big, "Receita Bruta",
-                        "Receita Bruta"), use_container_width=True)
-        st.plotly_chart(plot_indicator_bar(df_plot_big, "Custo Total",
-                        "Custo Total"), use_container_width=True)
-
-    with col2:
-        st.plotly_chart(plot_indicator_bar(df_plot_big, "Receita Líquida",
-                        "Receita Líquida"), use_container_width=True)
-        st.plotly_chart(plot_indicator_bar(df_plot_big, "Lucro Líquido",
-                        "Lucro Líquido"), use_container_width=True)
-
-    # Último gráfico centralizado
-    st.plotly_chart(plot_indicator_bar(df_plot_big, "Disponibilidade de Caixa",
-                    "Disponibilidade de Caixa"), use_container_width=True)
 
 
 with abas[0]:  # Aba "Gerencial"
     st.subheader("📑 Painel Gerencial")
 
-    tabela_pivot
+    #tabela_pivot
+    #indices_max
+
+    # Layout: 3 cards por linha
+    cols_per_row = 3
+    for i in range(0, len(indicadores), cols_per_row):
+        cols = st.columns(cols_per_row)
+        for col, indicador in zip(cols, indicadores[i:i+cols_per_row]):
+            col.markdown(f"""
+            <div style="
+                background-color: #f8f9fa;
+                padding: 25px;
+                border-radius: 15px;
+                box-shadow: 0 0 10px rgba(0,0,0,0.05);
+                height: 250px;
+            ">
+                <h4 style="margin-bottom: 5px;">{indicador['titulo']}</h4>
+                <p style="font-size: 13px; color: #555;">{indicador['descricao']}</p>
+                <p style="font-size: 12px; color: #777;"><b>Memória de Cálculo:</b> {indicador['memoria']}</p>
+                <h2 style="text-align:center; color:#000;">{indicador['valor']}</h2>
+            </div>
+            """, unsafe_allow_html=True)
