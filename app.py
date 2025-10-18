697b90dbf0baacdcb483d109c75f02bc200c623b
guilhermem0101 <guilherme.sou@hotmail.com>
2025-10-18 17:53:31 -0300

    limpeza front

--- app.py
+++ app.py
@@ -3,13 +3,9 @@ import plotly.express as px
 import pandas as pd
 import os
 import json
-from calcula_resultados import compute_indicators_from_files, extract_accounts
 from streamlit_elements import elements, mui, html, editor, nivo, media, lazy, sync, dashboard
 from utils.functions import processar_indicadores_financeiros, extract_accounts
-files = ["balancetes/balancete1.json"]
-result = compute_indicators_from_files(files)
-df_balancete = pd.json_normalize(
-    result["per_file"]).drop(columns=["file", "n_leaves"])
+
 
 # ======================== LÊ TODOS OS BALANCETES TEMPORAIS ========================
 # Caminho da pasta
@@ -45,8 +41,8 @@ df_hist = pd.DataFrame(all_rows)
 
 
 indicadores_historicos = processar_indicadores_financeiros(df_hist)
-ultimo_mes = indicadores_historicos.index.max()
-indices_max = indicadores_historicos.loc[indicadores_historicos.index == ultimo_mes]
+ultimo_mes = indicadores_historicos.index.max() #extrai ultimo mês
+indicadores_foto = indicadores_historicos.loc[indicadores_historicos.index == ultimo_mes] #filtra os indicadores do ultimo mês (visão foto)
 
 
 # Dados de exemplo
@@ -55,75 +51,47 @@ indicadores = [
         "titulo": "Endividamento Geral",
         "descricao": "Grau em que os Ativos Totais são financiados por recursos de terceiros.",
         "memoria": "Passivo circulante / Passivo circulante + Exigível Longo Prazo",
-        "valor": round(indices_max['Endividamento_Geral'].values[0], 2)
+        "valor": round(indicadores_foto['Endividamento_Geral'].values[0], 2)
     },
     {
         "titulo": "Endividamento",
         "descricao": "Relacionamento entre a posição do Patrimônio Líquido em relação aos Empréstimos.",
         "memoria": "Passivo circulante + Exigível a Longo Prazo / Ativo Total",
-        "valor": round(indices_max['Endividamento'].values[0], 2)
+        "valor": round(indicadores_foto['Endividamento'].values[0], 2)
     },
     {
         "titulo": "Liquidez Corrente",
         "descricao": "Capacidade de a empresa saldar suas dívidas a curto prazo, em até 360 dias.",
         "memoria": "Ativo Circulante / Passivo Circulante",
-        "valor": round(indices_max['Liquidez_Corrente'].values[0], 2)
+        "valor": round(indicadores_foto['Liquidez_Corrente'].values[0], 2)
     },
     {
         "titulo": "Solvência",
         "descricao": "Grau em que a empresa dispõe em Ativos Totais para o pagamento total de dívidas.",
         "memoria": "Ativo Total / Passivo Circulante + Exigível a Longo Prazo",
-        "valor": round(indices_max['Solvencia_Geral'].values[0], 2)
+        "valor": round(indicadores_foto['Solvencia_Geral'].values[0], 2)
     },
     {
         "titulo": "Liquidez Geral",
         "descricao": "Situação financeira da empresa a longo prazo, incluindo dívidas acima de 360 dias.",
         "memoria": "(Ativo Circulante + Realizável a Longo Prazo) / (Passivo Circulante + Exigível a Longo Prazo)",
-        "valor": round(indices_max['Liquidez_Geral'].values[0], 2)
+        "valor": round(indicadores_foto['Liquidez_Geral'].values[0], 2)
     },
     {
         "titulo": "Liquidez Imediata",
         "descricao": "Quanto a empresa possui de imediato no caixa, frente às obrigações exigíveis.",
         "memoria": "Disponível / Passivo Circulante",
-        "valor": round(indices_max['Liquidez_Imediata'].values[0], 2)
+        "valor": round(indicadores_foto['Liquidez_Imediata'].values[0], 2)
     }
 ]
 # ======================== INIT ========================
 
-df_balancete.columns = df_balancete.columns.str.replace("indicators.", "")
-receita_bruta = df_balancete.loc[0, "receita_bruta"]
-receita_liquida = df_balancete.loc[0, "receita_liquida"]
-lucro_bruito = df_balancete.loc[0, "lucro_bruto"]
-lucro_liquido = df_balancete.loc[0, "lucro_liquido"]
-disponibilidade_caixa = df_balancete.loc[0, "disponibilidade_caixa"]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+receita_bruta = indicadores_foto['Receita_Bruta'].values[0]
+receita_liquida = indicadores_foto['Receita_Líquida'].values[0]
+lucro_bruito = indicadores_foto['Lucro_Bruto'].values[0]
+lucro_liquido = indicadores_foto['Lucro_Líquido'].values[0]
+disponibilidade_caixa = indicadores_foto['Disponibilidade_Caixa'].values[0]
 
 
 
@@ -146,7 +114,35 @@ with st.sidebar:
 # ======================
 # TOPO COM ABAS
 # ======================
-abas = st.tabs(["Gerencial", "Contábil", "Projeção", "Balancete"])
+abas = st.tabs(["Gerencial", "Contábil", "Projeção", "Analítico"])
+
+
+with abas[0]:  # Aba "Gerencial"
+    st.subheader("📑 Painel Gerencial")
+
+    # indicadores_historicos
+    # indicadores_foto
+
+    # Layout: 3 cards por linha
+    cols_per_row = 3
+    for i in range(0, len(indicadores), cols_per_row):
+        cols = st.columns(cols_per_row)
+        for col, indicador in zip(cols, indicadores[i:i+cols_per_row]):
+            col.markdown(f"""
+            <div style="
+                background-color: #f8f9fa;
+                padding: 25px;
+                border-radius: 15px;
+                box-shadow: 0 0 10px rgba(0,0,0,0.05);
+                height: 250px;
+            ">
+                <h4 style="margin-bottom: 20px;">{indicador['titulo']}</h4>
+                <p style="font-size: 18px; color: #555;">{indicador['descricao']}</p>
+                <p style="font-size: 15px; color: #777;"><b>Memória de Cálculo:</b> {indicador['memoria']}</p>
+                <h2 style="text-align:center; color:#000;">{indicador['valor']}</h2>
+            </div>
+            """, unsafe_allow_html=True)
+
 
 with abas[1]:  # Aba "Contábil"
     st.subheader("📑 Painel Contábil")
@@ -235,30 +231,6 @@ with abas[1]:  # Aba "Contábil"
                         "Endividamento"), use_container_width=True)
 
     
-
-
-with abas[0]:  # Aba "Gerencial"
-    st.subheader("📑 Painel Gerencial")
-
-    #indicadores_historicos
-    #indices_max
-
-    # Layout: 3 cards por linha
-    cols_per_row = 3
-    for i in range(0, len(indicadores), cols_per_row):
-        cols = st.columns(cols_per_row)
-        for col, indicador in zip(cols, indicadores[i:i+cols_per_row]):
-            col.markdown(f"""
-            <div style="
-                background-color: #f8f9fa;
-                padding: 25px;
-                border-radius: 15px;
-                box-shadow: 0 0 10px rgba(0,0,0,0.05);
-                height: 250px;
-            ">
-                <h4 style="margin-bottom: 20px;">{indicador['titulo']}</h4>
-                <p style="font-size: 18px; color: #555;">{indicador['descricao']}</p>
-                <p style="font-size: 15px; color: #777;"><b>Memória de Cálculo:</b> {indicador['memoria']}</p>
-                <h2 style="text-align:center; color:#000;">{indicador['valor']}</h2>
-            </div>
-            """, unsafe_allow_html=True)
+with abas[3]:  # Aba "Gerencial"
+    st.subheader("📑 Analítico")
+    indicadores_historicos
